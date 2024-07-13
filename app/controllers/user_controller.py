bde768de1f468290d47d58617c94136967c70a01
Karol <karolbal66@gmail.com>
2024-07-13 19:59:58 +0200

    improving the project structure 3

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,28 +1,12 @@
-from app.models.user import UserModel
+from app.services.user import UserService
 
 class UserController:
     def __init__(self):
-        self.user_model = UserModel()
-        self.user_model.create_table()
+        self.user_service = UserService()
 
     def register_user(self, email, username, password, confirm_password):
-        if password != confirm_password:
-            return {"status": "error", "message": "Passwords do not match"}
-
-        existing_user = self.user_model.get_user_by_email(email)
-        if existing_user:
-            return {"status": "error", "message": "Email is already registered"}
-
-        existing_user_by_username = self.user_model.get_user_by_username(username)
-        if existing_user_by_username:
-            return {"status": "error", "message": "Username is already taken"}
-
-        self.user_model.add_user(email, username, password)
-        return {"status": "success", "message": "User registered successfully"}
+        return self.user_service.register_user(email, username, password, confirm_password)
 
     def login_user(self, identifier, password):
-        if self.user_model.validate_user(identifier, password):
-            return {"status": "success", "message": "Login successful"}
-        else:
-            return {"status": "error", "message": "Invalid email/username or password"}
+        return self.user_service.login_user(identifier, password)
 
---
+++ app/services/user.py
@@ -0,0 +1,28 @@
+from app.models.user import UserModel
+
+class UserService:
+    def __init__(self):
+        self.user_model = UserModel()
+        self.user_model.create_table()
+
+    def register_user(self, email, username, password, confirm_password):
+        if password != confirm_password:
+            return {"status": "error", "message": "Passwords do not match"}
+
+        existing_user = self.user_model.get_user_by_email(email)
+        if existing_user:
+            return {"status": "error", "message": "Email is already registered"}
+
+        existing_user_by_username = self.user_model.get_user_by_username(username)
+        if existing_user_by_username:
+            return {"status": "error", "message": "Username is already taken"}
+
+        self.user_model.add_user(email, username, password)
+        return {"status": "success", "message": "User registered successfully"}
+
+    def login_user(self, identifier, password):
+        if self.user_model.validate_user(identifier, password):
+            return {"status": "success", "message": "Login successful"}
+        else:
+            return {"status": "error", "message": "Invalid email/username or password"}
+
